18a7024788ab1aea70566a4f4d42f7e9f33637ef
Max Malkin <maxim_malkin@outlook.com>
2025-10-24 17:21:24 -0600

    feat: add basic cpu and memory tracking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use sysinfo::{CpuExt, System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+
+    sys.refresh_all();
+
+    // format usage to one decimal place
+    println!("CPU Usage: {:.1}%", sys.global_cpu_info().cpu_usage());
+
+    println!(
+        "Memory: {} MB / {} MB",
+        sys.used_memory() / 1024 / 1024, // convert to MB
+        sys.total_memory() / 1024 / 1024
+    )
 }
