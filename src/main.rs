2ae25ebe026fa3c89280946ae718433248b74d37
Max Malkin <maxim_malkin@outlook.com>
2025-10-24 17:33:56 -0600

    chore: clean variables to not call sys functions more than once

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,17 @@ fn main() {
 
         // format usage to one decimal place
         println!("CPU Usage: {:.1}%", sys.global_cpu_info().cpu_usage());
+
+        let used_mem = sys.used_memory() / 1024 / 1024;
+        let total_mem = sys.total_memory() / 1024 / 1024;
+        let mem_used_percent = (used_mem as f64 / total_mem as f64) * 100.0;
+
         println!(
-            "Memory: {} MB / {} MB",
-            sys.used_memory() / 1024 / 1024, // convert to MB
-            sys.total_memory() / 1024 / 1024
-        )
+            "Memory: {} MB / {} MB, {:.1}%",
+            used_mem, total_mem, mem_used_percent
+        );
+
+        // update every 1 second.
+        thread::sleep(Duration::from_secs(1));
     }
 }
