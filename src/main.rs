d243191f39bd16653a3688ca29e834fe5da17a39
Max Malkin <maxim_malkin@outlook.com>
2025-10-24 17:33:22 -0600

    feat: add usage to loop

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use std::thread;
+use std::time::Duration;
 use sysinfo::{CpuExt, System, SystemExt};
 
 fn main() {
     let mut sys = System::new_all();
 
-    sys.refresh_all();
+    // infinite loop to allow realtime updates
+    loop {
+        print!("\x1B[2J\x1B[1;1H");
+        sys.refresh_all();
 
-    // format usage to one decimal place
-    println!("CPU Usage: {:.1}%", sys.global_cpu_info().cpu_usage());
+        println!("Monitor Running...");
 
-    println!(
-        "Memory: {} MB / {} MB",
-        sys.used_memory() / 1024 / 1024, // convert to MB
-        sys.total_memory() / 1024 / 1024
-    )
+        // format usage to one decimal place
+        println!("CPU Usage: {:.1}%", sys.global_cpu_info().cpu_usage());
+        println!(
+            "Memory: {} MB / {} MB",
+            sys.used_memory() / 1024 / 1024, // convert to MB
+            sys.total_memory() / 1024 / 1024
+        )
+    }
 }
