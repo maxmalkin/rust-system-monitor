f6330ae2d026240f1d5cbc873d250026ac034666
Max Malkin <maxim_malkin@outlook.com>
2025-10-24 17:46:13 -0600

    feat: add disk usage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::thread;
 use std::time::Duration;
-use sysinfo::{CpuExt, System, SystemExt};
+use sysinfo::{CpuExt, DiskExt, System, SystemExt};
 
 fn main() {
     let mut sys = System::new_all();
@@ -11,10 +11,10 @@ fn main() {
         sys.refresh_all();
 
         println!("Monitor Running...");
+        println!();
 
         // format usage to one decimal place
         println!("CPU Usage: {:.1}%", sys.global_cpu_info().cpu_usage());
-
         let used_mem = sys.used_memory() / 1024 / 1024;
         let total_mem = sys.total_memory() / 1024 / 1024;
         // cast memory to float to calculate percentage accurately
@@ -24,6 +24,23 @@ fn main() {
             "Memory: {} MB / {} MB, {:.1}%",
             used_mem, total_mem, mem_used_percent
         );
+
+        for disk in sys.disks() {
+            let name = disk.name().to_str().unwrap_or("Name not found."); // default if name not found
+            let total_space = disk.total_space() / 1024 / 1024 / 1024; // convert disk space to GB
+            let available_space = disk.available_space() / 1024 / 1024 / 1024;
+            let used_space = total_space - available_space;
+            if total_space > 0 {
+                let used_space_percent = (used_space as f64 / total_space as f64) * 100;
+            } else {
+                let used_space_percent = 0;
+            }
+
+            println!(
+                "{}: {} GB / {} GB, {:.1}%",
+                name, used_space, total_space, used_space, used_space_percent
+            );
+        }
 
         // update every 1 second.
         thread::sleep(Duration::from_secs(1));
