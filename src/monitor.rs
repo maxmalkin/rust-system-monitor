e976c5fa86284a471e50be399aeda60f5287f032
Max Malkin <maxim_malkin@outlook.com>
2025-10-25 12:15:33 -0600

    feat: add rates to monitor

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,5 +1,6 @@
 use crate::history::History;
-use sysinfo::{CpuExt, System, SystemExt};
+use sysinfo::{CpuExt, System, SystemExt, DiskExt, NetworkExt};
+use std::collections::HashMap;
 
 pub struct SystemData {
     pub cpu_usage: f32,
@@ -20,6 +21,35 @@ pub fn collect_system_data(sys: &System) -> SystemData {
         total_mem_mb,
         mem_percent,
     }
+}
+
+pub fn collect_network_data(sys: &System) -> HashMap<String, (u64, u64)> {
+    let mut data = HashMap::new();
+
+    for (name, net_data) in sys.networks() {
+        data.insert(
+            name.clone(),
+            (net_data.total_received(), net_data.total_transmitted()),
+        );
+    }
+
+    data
+}
+
+pub fn collect_disk_data(sys: &System) -> HashMap<String, (u64, u64)> {
+    let mut data = HashMap::new();
+
+    for disk in sys.disks() {
+    	// disk name as key
+    	let name = disk.name().to_str().unwrap_or("Unknown").to_string();
+
+        data.insert(
+            name,
+            (disk.total_space(), disk.available_space()),
+        );
+    }
+
+    data
 }
 
 pub fn system_data_to_history(data: &SystemData) -> History {
